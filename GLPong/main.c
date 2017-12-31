1c630d18f1e8939d72e2343e789f2d34eb9a21dc
神崎航 <wataru@macbook.local>
2017-12-30 21:56:23 -0500

    Initial Commit

---
+++ GLPong/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  GLPong
+//
+//  Created by 神崎航 on 2017/12/30.
+//  Copyright © 2017年 神崎航. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
